8268674e395769296add82bc6d43270d2b81d73b
Ruben <ssrcruz@gmail.com>
2015-12-08 23:58:37 -0500

    Make test 3-6 pass

--- department.rb
+++ department.rb
@@ -1,10 +1,17 @@
 class Department
+  attr_reader :roster, :name # attr_reader creates a method and an instance variable
   def initialize(name)
     @name = name
+    @roster = []
   end
 
-  def add_employee
-    name = "Computer Science"
+  def add_employee(employee)
+    @roster << employee
   end
+
+  def total_salary
+    add_employee
+  end
+
 
 end
